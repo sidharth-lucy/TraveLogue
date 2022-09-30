fda07637ae849adb15bed0d9ebb54e932edbce24
Sidharth kumar <sidharthkr00sk@gmail.com>
2022-09-30 17:33:01 +0530

    second commit added the default 404 page and added an svg for that

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -23,3 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL , document_root= settings.MEDIA_ROOT)
+
+handler404 = "mysite.views.page_not_found"
